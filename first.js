16cf90d209a0052dae0b1de1d044c2a8d9acdbc3
Samip Nepal <samipnepal5@gmail.com>
2024-07-09 22:37:03 +1000

    Simple example of events which is use to trigger somethings or change in state

---
+++ first.js
@@ -0,0 +1,8 @@
+let sai=document.querySelector(".di")
+sai.onclick = () => {
+    console.log("Clicked");
+};
+let saii=document.querySelector("#heading")
+saii.onmouseover = () => {
+    console.log("This is heading");
+};
