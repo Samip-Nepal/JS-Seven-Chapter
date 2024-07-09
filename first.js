1e2cc98d18916a1978f0ead7c0ceb0b6e600396b
Samip Nepal <samipnepal5@gmail.com>
2024-07-09 23:08:20 +1000

    Adding best way of events handling by events listeners

--- first.js
+++ first.js
@@ -6,9 +6,17 @@ sai.onclick = (event) => {
     console.log("Clicked");
 };
 let saii=document.querySelector("#heading")
-saii.onmouseover = () => {
+/*saii.onmouseover = () => {
     console.log(event.target);
     console.log(event.type);
     console.log(event.clientX ,event.clientY);
     console.log("This is heading");
-};
+};*/
+//Handing events by event listeners
+saii.addEventListener("click", (event) => {
+    console.log("This is heading");
+    console.log(event.target);
+    console.log(event.type);
+    console.log(event.clientX ,event.clientY);
+
+});
