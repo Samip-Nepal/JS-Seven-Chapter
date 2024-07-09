a3d70c5abd9dc7401af024520ec84c237cf489de
Samip Nepal <samipnepal5@gmail.com>
2024-07-09 22:48:58 +1000

    Event object which has acess to event properties

--- first.js
+++ first.js
@@ -1,8 +1,14 @@
 let sai=document.querySelector(".di")
-sai.onclick = () => {
+sai.onclick = (event) => {
+    console.log(event.target);
+    console.log(event.type);
+    console.log(event.clientX ,event.clientY);
     console.log("Clicked");
 };
 let saii=document.querySelector("#heading")
 saii.onmouseover = () => {
+    console.log(event.target);
+    console.log(event.type);
+    console.log(event.clientX ,event.clientY);
     console.log("This is heading");
 };
